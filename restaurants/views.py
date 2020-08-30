5db43b7b10a9a77818ca955e60c79aa73f66b427
DarthHamza <hamzamakia@gmail.com>
2020-08-30 09:53:15 +0300

    completed task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,18 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
-
+        "my_list": [
+            {"restaurant_name": "Aroma", "food_type": "Italian, I think."},
+            {"restaurant_name": "Tokyo", "food_type": "Japanese Food"},
+            {"restaurant_name": "Fire Grill", "food_type": "Mexican Food"},
+        ]
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
     context = {
-
+        "my_object": {"restaurant_name": "Tokyo", "food_type": "Japanese Food"}
     }
     return render(request, 'detail.html', context)
